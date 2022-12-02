abb935e07f58f9dd18efc5771e2e44d384c7eb43
MYEONGGI <smgsmg226@gmail.com>
2022-12-03 01:37:33 +0900

    myeonggi commit

--- src/page/index.js
+++ src/page/index.js
@@ -3,7 +3,15 @@ import React from 'react'
 const index = () => {
   return (
     <div>
-      test
+      test 
+      {/*make flexbox*/}
+      <div className="flexbox">
+        <div className="flexbox__item">1</div>
+        <div className="flexbox__item">2</div>
+        <div className="flexbox__item">3</div>
+      </div>
+
+      
     </div>
   )
 }
